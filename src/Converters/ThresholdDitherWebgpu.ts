8c617caa5c06d12ce6fffbabcf477f2028ca0bac
UpmarinPi <upmarin.video@gmail.com>
2025-11-14 08:27:58 +0900

    組織的ディザリングの組み込み

--- src/Converters/ThresholdDitherWebgpu.ts
+++ src/Converters/ThresholdDitherWebgpu.ts
@@ -52,6 +52,7 @@ export class ThresholdDitherWebgpu {
             usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ,
         });
 
+
         const bindGroupLayout = computePipeline.getBindGroupLayout(0);
 
         const bindGroup = this.device.createBindGroup({
@@ -69,5 +70,88 @@ export class ThresholdDitherWebgpu {
 
         passEncoder.setPipeline(computePipeline);
         // passEncoder.
+    }
+    async SetupStorageBuffer(pipeline: GPUComputePipeline){
+        if(!this.device) {
+            return;
+        }
+
+        // buffer定義
+        const inputTextureBuffer = this.device.createBuffer({
+            size: 1, // todo
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST
+        });
+        const inputSamplerBuffer = this.device.createBuffer({
+            size: 1,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST
+        });
+        const imageSizeBuffer = this.device.createBuffer({
+            size: 1,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST
+        });
+        const outputTextureBuffer = this.device.createBuffer({
+            size: 1,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC
+        });
+        const usableColorListBuffer = this.device.createBuffer({
+            size: 1,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST
+        });
+        const usableColorListNumBuffer = this.device.createBuffer({
+            size: 1,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST
+        });
+        const thresholdMapBuffer = this.device.createBuffer({
+            size: 1,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST
+        });
+        const thresholdMapSizeBuffer = this.device.createBuffer({
+            size: 1,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST
+        })
+
+        // 入力画像
+        const bindGroup0 = this.device.createBindGroup({
+            layout: pipeline.getBindGroupLayout(0),
+            entries: [
+                { binding: 0, resource: { buffer: inputTextureBuffer },},
+                { binding: 1, resource: { buffer: inputSamplerBuffer },},
+                { binding: 2, resource: { buffer: imageSizeBuffer },},
+            ]
+        });
+
+        // 出力画像
+        const bindGroup1 = this.device.createBindGroup({
+            layout: pipeline.getBindGroupLayout(1),
+            entries: [
+                { binding: 0, resource: { buffer: outputTextureBuffer },},
+            ]
+        });
+
+        // 色リスト
+        const bindGroup2 = this.device.createBindGroup({
+            layout: pipeline.getBindGroupLayout(2),
+            entries: [
+                { binding: 0, resource: { buffer: usableColorListBuffer },},
+                { binding: 1, resource: { buffer: usableColorListNumBuffer },},
+            ]
+        });
+
+        // 閾値系
+        const bindGroup3 = this.device.createBindGroup({
+            layout: pipeline.getBindGroupLayout(3),
+            entries: [
+                { binding: 0, resource: { buffer: thresholdMapBuffer },},
+                { binding: 1, resource: { buffer: thresholdMapSizeBuffer },},
+            ]
+        });
+//         // GPUで使用するバッファーを作る
+//         const storageBuffer = this.device.createBuffer({
+//             size: storageData.byteLength,
+//             usage:
+//                 GPUBufferUsage.COPY_DST | GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC,
+//         });
+// // 非同期に値を転送
+//         this.device.queue.writeBuffer(storageBuffer, 0, storageData);
     }
 }
